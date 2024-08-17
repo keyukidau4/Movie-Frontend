90926b54d34b7c296759cf1a9d63e5cbaedf6b1e
keyukidau4 <keyukidau4@gmail.com>
2024-08-17 16:30:02 +0900

    update link api

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -5,7 +5,7 @@ import axios from "axios";
 const instance = axios.create({
   // baseURL: 'https://api.themoviedb.org/3'
   // baseURL: "http://localhost:8080/api/",
-  baseURL: "https://movie-backend-three.vercel.app/",
+  baseURL: "https://movie-backend-three.vercel.app/api/",
 });
 
 // instance.get('/foo-bar');
